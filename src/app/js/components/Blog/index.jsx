4a582c9b20e4cff2dc0a9ad07c2ec491082066b0
Anastasiia Grigorenko <nastenka28.10.96@gmail.com>
2020-06-21 02:10:25 +0300

    feature: rendered blog items

--- src/app/js/components/Blog/index.jsx
+++ src/app/js/components/Blog/index.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import NewsCard from "./NewsCard"
 import PropTypes from 'prop-types';
+import data from "./newsElems.json"
 
 const index = props => {
+    const cardsList = data.data.map(card => (<NewsCard card={card}></NewsCard>));
+
     return (
         <div>
-            <div className="card-columns">
-
-
-
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
-
-
-
-
+            <div className="container">
+                { cardsList }
             </div>
         </div>
     );
