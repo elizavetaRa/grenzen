be0452390aba3c9c166bac2423e7b5c51f6f6073
Elizaveta <svillyfly@mail.ru>
2020-07-15 20:11:39 +0200

    test footer safari

--- src/app/js/components/footer.jsx
+++ src/app/js/components/footer.jsx
@@ -20,84 +20,84 @@ class footer extends Component {
 
   render() {
     return (
-      <div className="d-flex flex-column">
-
-        <footer className="page-footer bg-dark font-small mdb-color lighten-3 pt-4">
 
 
-          <div className="container-width">
-
-            <br></br>
-
-            <div className="row">
-
-              <div className="col-sm-10"><h4 className="text-center text-white">Gefördert von:</h4></div>
-              {window.innerWidth >= 992 && <div className="col-sm-2">
-                <h4 className="text-center text-white"> Unterstützt durch:</h4></div>}
-            </div>
-
-            <div className="row">
-
-              <div className="col-sm thumb">
-
-                <a href="https://www.stw.berlin/" target="_blank" rel="noopener noreferrer" >
-                  <Card className="h-100 p-2 justify-content-center">
-                    <img className="img-fluid" src={pic1} alt="Studierendenwerk Berlin" />
-                  </Card></a>
-
-              </div>
-
-              <div className="col-sm thumb">
-                <a href="https://gwk.udk-berlin.de/fachschaftsrat/" target="_blank" rel="noopener noreferrer" >
-                  <Card className="h-100  p-2 justify-content-center">
-                    <img className="img-fluid" src={pic2} alt="Fachschaftsrat für Gesellschafts- und Wirtschaftskommunikation" />
-                  </Card></a>
-
-              </div>
-              <div className="col-sm thumb">
-                <a href="https://www.hug-berlin.de/" target="_blank" rel="noopener noreferrer" >
-                  <Card className="h-100  p-2 justify-content-center">
-                    <img className="img-fluid" src={pic3} alt="Humbold Universitätsgesellschaft" />
-                  </Card></a>
-              </div>
-              <div className="col-sm thumb">
-                <a href="https://bolognalab.hu-berlin.de/de" target="_blank" rel="noopener noreferrer" >
-                  <Card className="h-100  p-2 justify-content-center">
-                    <img className="img-fluid" src={pic4} alt="Bologna Lab" />
-                  </Card></a>
-              </div>
-
-              <div className="col-sm thumb">
-                <a href="https://www.hu-berlin.de/de" target="_blank" rel="noopener noreferrer" >
-                  <Card className="h-100  p-2 justify-content-center">
-                    <img className="img-fluid" src={pic5} alt="Humbold Universität zu Berlin" />
-                  </Card></a>
-              </div>
-
-              <div className="col-sm thumb">
-                {window.innerWidth < 992 &&
-                  <h4 className="text-center text-white"> Unterstützt durch:</h4>}
-                <a href="https://www.bmbf.de/" target="_blank" rel="noopener noreferrer" >
-                  <Card className="h-100  p-2 justify-content-center">
-                    <img className="img-fluid" src={pic6} alt="Bundesministerium für Bildung und Forschung" />
-                  </Card></a>
-              </div>
+      <footer className="page-footer bg-dark font-small mdb-color lighten-3 pt-4">
 
 
-            </div>
+        <div className="container-width">
 
-            <br></br>
+          <br></br>
 
-            <div className="row">
-              <div className="col text-center text-white">
-                <Link to={`/datenschutz`}>Datenschutz</Link>
-                <br></br>
-                <Link to={`/impressum`}>Impressum</Link></div>
-            </div>
-            <br></br><br></br>
+          <div className="row">
+
+            <div className="col-sm-10"><h4 className="text-center text-white">Gefördert von:</h4></div>
+            {window.innerWidth >= 992 && <div className="col-sm-2">
+              <h4 className="text-center text-white"> Unterstützt durch:</h4></div>}
           </div>
-        </footer>
-      </div>
+
+          <div className="row">
+
+            <div className="col-sm thumb">
+
+              <a href="https://www.stw.berlin/" target="_blank" rel="noopener noreferrer" >
+                <Card className="h-100 p-2 justify-content-center">
+                  <img className="img-fluid" src={pic1} alt="Studierendenwerk Berlin" />
+                </Card></a>
+
+            </div>
+
+            <div className="col-sm thumb">
+              <a href="https://gwk.udk-berlin.de/fachschaftsrat/" target="_blank" rel="noopener noreferrer" >
+                <Card className="h-100  p-2 justify-content-center">
+                  <img className="img-fluid" src={pic2} alt="Fachschaftsrat für Gesellschafts- und Wirtschaftskommunikation" />
+                </Card></a>
+
+            </div>
+            <div className="col-sm thumb">
+              <a href="https://www.hug-berlin.de/" target="_blank" rel="noopener noreferrer" >
+                <Card className="h-100  p-2 justify-content-center">
+                  <img className="img-fluid" src={pic3} alt="Humbold Universitätsgesellschaft" />
+                </Card></a>
+            </div>
+            <div className="col-sm thumb">
+              <a href="https://bolognalab.hu-berlin.de/de" target="_blank" rel="noopener noreferrer" >
+                <Card className="h-100  p-2 justify-content-center">
+                  <img className="img-fluid" src={pic4} alt="Bologna Lab" />
+                </Card></a>
+            </div>
+
+            <div className="col-sm thumb">
+              <a href="https://www.hu-berlin.de/de" target="_blank" rel="noopener noreferrer" >
+                <Card className="h-100  p-2 justify-content-center">
+                  <img className="img-fluid" src={pic5} alt="Humbold Universität zu Berlin" />
+                </Card></a>
+            </div>
+
+            <div className="col-sm thumb">
+              {window.innerWidth < 992 &&
+                <h4 className="text-center text-white"> Unterstützt durch:</h4>}
+              <a href="https://www.bmbf.de/" target="_blank" rel="noopener noreferrer" >
+                <Card className="h-100  p-2 justify-content-center">
+                  <img className="img-fluid" src={pic6} alt="Bundesministerium für Bildung und Forschung" />
+                </Card></a>
+            </div>
+
+
+          </div>
+
+          <br></br>
+
+          <div className="row">
+            <div className="col text-center text-white">
+              <Link to={`/datenschutz`}>Datenschutz</Link>
+              <br></br>
+              <Link to={`/impressum`}>Impressum</Link></div>
+          </div>
+          <br></br><br></br>
+        </div>
+      </footer>
+
 
 
     );
